ebf9364952542cd78aede800ac06575ff76f7377
EC2 Default User <ec2-user@ip-172-31-49-34.ec2.internal>
2023-07-09 05:58:14 +0000

    Modify config.tf file

--- terraform/instance/config.tf
+++ terraform/instance/config.tf
@@ -20,30 +20,30 @@ data "aws_vpc" "default" {
 #Create S3 Bucket to stored Terraform state
 terraform {
   backend "s3" {
-    bucket = "assignment-2"
+    bucket = "nila-clo835-assignment-2"
     key    = "prod/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 #Create a ECR repositorie named "my-app"
-resource "aws_ecr_repository" "app_repository" {
-  name                 = "my-app"
-  image_tag_mutability = "MUTABLE"
+#resource "aws_ecr_repository" "app_repository" {
+#  name                 = "my-app"
+#  image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
+#  image_scanning_configuration {
+ #   scan_on_push = true
+ # }
+#}
 
 #Create a ECR repositorie named "my-db"
-resource "aws_ecr_repository" "db_repository" {
-  name                 = "my-db"
-  image_tag_mutability = "MUTABLE"
+#resource "aws_ecr_repository" "db_repository" {
+#  name                 = "my-db"
+#  image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
+#  image_scanning_configuration {
+#    scan_on_push = true
+#  }
+#}
 
 #Testing
